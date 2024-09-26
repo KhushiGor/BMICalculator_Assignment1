4833f2b7d4e385bfd4c1ca57c6afaabc2313eb6d
KhushiGor <khushigorpersonal@gmail.com>
2024-09-26 19:19:07 -0400

    input validation

--- BMICalculator_Assignment1/ViewController.swift
+++ BMICalculator_Assignment1/ViewController.swift
@@ -56,18 +56,21 @@ class ViewController: UIViewController {
             if let height = height1Input.text, let weight = weightInput.text {
                 if !height.isEmpty && !weight.isEmpty {
                     if let numHeight = Double(height), let numWeight = Double(weight){
-                        let bmi : Double = (numWeight / ((numHeight / 100) * (numHeight / 100)))
+                        if numHeight >= 0 && numWeight >= 0 {
+                            let bmi : Double = (numWeight / ((numHeight / 100) * (numHeight / 100)))
+                            
+                            bmiNumber.text = String(format: "%.2f", bmi)
+                            if bmi <= 18.5 {
+                                conclusion.text = "UnderWeight"
+                            } else if bmi > 18.5 && bmi < 24.9 {
+                                conclusion.text = "Normal Weight"
+                            } else if bmi > 25 && bmi < 29.9 {
+                                conclusion.text = "OverWeight"
+                            } else {
+                                conclusion.text = "Obese"
+                            }
+                        }else {bmiNumber.text = "Invalid Input"}
                         
-                        bmiNumber.text = String(format: "%.2f", bmi)
-                        if bmi <= 18.5 {
-                            conclusion.text = "UnderWeight"
-                        } else if bmi > 18.5 && bmi < 24.9 {
-                            conclusion.text = "Normal Weight"
-                        } else if bmi > 25 && bmi < 29.9 {
-                            conclusion.text = "OverWeight"
-                        } else {
-                            conclusion.text = "Obese"
-                        }
                     }
                     
                 }
@@ -76,19 +79,22 @@ class ViewController: UIViewController {
             if let heightInFeet = height1Input.text, let heightInInches = height2Input.text, let weight = weightInput.text {
                 if !heightInFeet.isEmpty && !heightInInches.isEmpty && !weight.isEmpty {
                     if let numHeightInFeet = Double(heightInFeet), let numHeightInInches = Double(heightInInches), let numWeight = Double(weight){
-                        let totalInches = (numHeightInFeet * 12) + numHeightInInches
-                        let heightInMeters = totalInches * 0.0254
-                        let bmi : Double = (numWeight * 0.435) / ((heightInMeters) * (heightInMeters))
-                        bmiNumber.text = String(format: "%.2f", bmi)
-                        if bmi <= 18.5 {
-                            conclusion.text = "UnderWeight"
-                        } else if bmi > 18.5 && bmi < 24.9 {
-                            conclusion.text = "Normal Weight"
-                        } else if bmi > 25 && bmi < 29.9 {
-                            conclusion.text = "OverWeight"
-                        } else {
-                            conclusion.text = "Obese"
-                        }
+                        if numHeightInFeet >= 0 && numWeight >= 0 && numHeightInInches >= 0 {
+                            let totalInches = (numHeightInFeet * 12) + numHeightInInches
+                            let heightInMeters = totalInches * 0.0254
+                            let bmi : Double = (numWeight * 0.435) / ((heightInMeters) * (heightInMeters))
+                            bmiNumber.text = String(format: "%.2f", bmi)
+                            if bmi <= 18.5 {
+                                conclusion.text = "UnderWeight"
+                            } else if bmi > 18.5 && bmi < 24.9 {
+                                conclusion.text = "Normal Weight"
+                            } else if bmi > 25 && bmi < 29.9 {
+                                conclusion.text = "OverWeight"
+                            } else {
+                                conclusion.text = "Obese"
+                            }
+                        }else {bmiNumber.text = "Invalid Input"}
+                        
                     }
                     
                 }
